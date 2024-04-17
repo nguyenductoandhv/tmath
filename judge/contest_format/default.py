f7c1ee113adfa7be664b16dbb1b094e6e1a44fe7
patpro28 <phamanhtu1996@gmail.com>
2024-04-17 17:36:47 +0700

    #34 - Change dark mode for ranking contest.

--- judge/contest_format/default.py
+++ judge/contest_format/default.py
@@ -66,7 +66,7 @@ class DefaultContestFormat(BaseContestFormat):
     def display_participation_result(self, participation):
         return format_html(
             u'''<td class="sticky right-0 default_format">
-                <a class="font-bold text-black" href="{url}">{points}<div class="solving-time">{cumtime}</div></a>
+                <a class="font-bold text-black dark:text-white" href="{url}">{points}<div class="solving-time">{cumtime}</div></a>
             </td>''',
             url=reverse('contest_all_user_submissions',
                         args=[self.contest.key, participation.user.user.username]),
